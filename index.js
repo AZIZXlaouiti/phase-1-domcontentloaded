3808846b9732a2d6ee0e1ded90d2645dfaeefa15
AZIZXlaouiti <azizrocke@gmail.com>
2021-06-15 10:09:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",()=>{
+    const par = document.querySelector('p');
+    par.innerHTML = 'This is really cool!';
+   console.log('contentloaded')
+});
+
